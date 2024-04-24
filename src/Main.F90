ff9924fd961b9bd21caf104340a0e7ea3b63eb46
FinnWindfly <finnwindfly@gmail.com>
2024-04-24 11:32:34 +0800

    add SoilConstantMod module file

--- src/Main.F90
+++ src/Main.F90
@@ -2,8 +2,10 @@ program main
 
     use ModelParameterMod
     use SoilParameterMod
+    use SoilConstantMod
 
     print *, author_name
     print *, nlevsoi
+    print *, cpliq
 
 end program main
