bf8a7e8071a867e442c661bf8be7001d8d623f6e
FinnWindfly <finnwindfly@gmail.com>
2024-04-24 14:13:32 +0800

    add TridiagonalMod module file

--- src/Main.F90
+++ src/Main.F90
@@ -3,9 +3,29 @@ program main
     use ModelParameterMod
     use SoilParameterMod
     use SoilConstantMod
+    use TridiagonalMod
+
+    integer, parameter :: n = 5  ! 矩阵大小
+    real(kind=16) :: a(n), b(n), c(n), r(n), u(n)  ! 矩阵和解向量
+    integer :: i  ! 循环变量
 
     print *, author_name
     print *, nlevsoi
     print *, cpliq
+
+    print *, "here start using Tridiagonal Matrix solution:"
+
+    ! 初始化矩阵和外力项
+    a = 1.0
+    b = 2.0
+    c = 1.0
+    r = (/ (real(i), i=1,n) /)
+
+    ! 调用Tridiagonal模块中的子程序解决矩阵
+    call Tridiagonal(1, n, 1, a, b, c, r, u)
+
+    ! 打印解向量
+    print *, "Solution Vector u:"
+  print *, u
 
 end program main
---
+++ src/TridiagonalMod.F90
@@ -0,0 +1,63 @@
+!--------------------------------------------------------------------------------------------------
+module TridiagonalMod
+
+! !PUBLIC TYPES:
+  implicit none
+  save
+
+! !PUBLIC MEMBER FUNCTIONS:
+  public :: Tridiagonal
+
+contains
+
+!
+! SUBROUTINE: Tridiagonal
+!
+! Tridiagonal matrix solution
+! using for solve soil one-dimension heat transfer equation &
+! soil one-dimension water transfer equation
+
+  subroutine Tridiagonal(lbj, ubj, jtop, a, b, c, r, u)
+
+! !ARGUMENTS:
+    implicit none
+    integer , intent(in)         :: lbj, ubj               ! lbinning and ubing level indices
+    integer , intent(in)         :: jtop                   ! top level for each column
+    real(kind=16), intent(in)    :: a(lbj:ubj)             ! "a" left off diagonal oftridiagonal matrix
+    real(kind=16), intent(in)    :: b(lbj:ubj)             ! "b" diagonal column for tridiagonal matrix
+    real(kind=16), intent(in)    :: c(lbj:ubj)             ! "c" right off diagonal tridiagonal matrix
+    real(kind=16), intent(in)    :: r(lbj:ubj)             ! "r" forcing term of tridiagonal matrix
+    real(kind=16), intent(inout) :: u(lbj:ubj)             ! solution
+!
+! !REVISION HISTORY:
+! Finn Windfly, Initial code, 2023.12.12
+
+! !LOCAL VARIABLES:
+    integer       :: j                 !indices
+    real(kind=16) :: gam(lbj:ubj)      !temporary
+    real(kind=16) :: bet               !temporary
+
+    ! solve the matrix
+    bet = b(jtop)
+
+    do j = lbj, ubj
+      if (j >= jtop) then
+        if (j == jtop) then
+          u(j) = r(j) / bet
+        else
+          gam(j) = c(j-1) / bet
+          bet = b(j) - a(j) * gam(j)
+          u(j) = (r(j) - a(j)*u(j-1)) / bet
+        end if
+      end if
+    end do
+
+    do j = ubj-1,lbj,-1
+      if (j >= jtop) then
+        u(j) = u(j) - gam(j+1) * u(j+1)
+      end if
+    end do
+
+  end subroutine Tridiagonal
+
+end module TridiagonalMod
